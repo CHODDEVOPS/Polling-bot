e6740767da269d2a13ab4eac8afdd2b1989c773e
divinefavourodion <divineod9@gmail.com>
2023-04-20 21:27:04 +0200

    Refactored Availability checker

--- availability_notifier.py
+++ availability_notifier.py
@@ -2,14 +2,13 @@ import time
 import requests
 import datetime as dt
 from bs4 import BeautifulSoup
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+# from telegram import Update, ForceReply
+# from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Global constants
 TELEGRAM_BOT_TOKEN = ""
 TELEGRAM_CHAT_ID = ""
-URL1 = "https://termin.bremen.de/termine/suggest?mdt=701&select_cnc=1&cnc-8568=0&cnc-8580=0&cnc-8582=0&cnc-8598=0&cnc-8583=0&cnc-8587=0&cnc-8597=0&cnc-8579=0&cnc-8596=0&cnc-8599=0&cnc-8600=0&cnc-8797=0&cnc-8790=1&cnc-8588=0&cnc-8591=0&cnc-8798=0&cnc-8569=0&cnc-8570=0&cnc-8571=0&cnc-8572=0&cnc-8574=0&cnc-8576=0&cnc-8595=0&cnc-8573=0&cnc-8593=0&cnc-8789=0&cnc-8577=0&cnc-8575=0&cnc-8578=0&cnc-8590=0&cnc-8585=0&cnc-8581=0"
-URL2 = "https://termin.bremen.de/termine/suggest?mdt=705&select_cnc=1&cnc-8665=0&cnc-8674=0&cnc-8675=0&cnc-8676=0&cnc-8690=0&cnc-8678=0&cnc-8679=0&cnc-8673=0&cnc-8689=0&cnc-8691=0&cnc-8692=0&cnc-8693=0&cnc-8688=0&cnc-8804=0&cnc-8792=1&cnc-8682=0&cnc-8684=0&cnc-8664=0&cnc-8666=0&cnc-8667=0&cnc-8668=0&cnc-8669=0&cnc-8670=0&cnc-8671=0&cnc-8672=0&cnc-8677=0&cnc-8791=0&cnc-8686=0&cnc-8802=0"
+URL1 = "https://termin.bremen.de/termine/"
 POLLING_INTERVAL = 60  # Polling interval in seconds
 
 # Dates to check for available slots
@@ -36,26 +35,60 @@ def check_website(url: str, dates_list: list) -> bool:
 
 
 def send_notification(text: str):
-    updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
-    updater.bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=text)
+    # updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
+    # updater.bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=text)
+    pass
 
 
 def polling_bot(dates_list: list):
-    while True:
-        is_available_url1 = check_website(URL1, dates_list)  # Check Bsc-mitte
-        is_available_url2 = check_website(URL2, dates_list)  # Check Nord
+    # while True:
+    #     is_available_url1 = check_website(URL1, dates_list)  # Check Bsc-mitte
+    #
+    #     if is_available_url1:
+    #             print("Success!")
+    #             send_notification(f"Hi there, early slots are now available to register your apartment on: {URL1}")
+    #     else:
+    #         print(f"Information not available yet. Checking again in {POLLING_INTERVAL} seconds...")
+    #         time.sleep(POLLING_INTERVAL)
 
-        if is_available_url1 or is_available_url2:
-            if is_available_url1:
-                print("Success!")
-                # send_notification(f"Hi there, early slots are now available to register your apartment on: {URL1}")
-            if is_available_url2:
-                print("Success2!")
-                # send_notification(f"Hi there, early slots are now available to register your apartment on: {URL2}")
-            break
-        else:
-            print(f"Information not available yet. Checking again in {POLLING_INTERVAL} seconds...")
-            time.sleep(POLLING_INTERVAL)
+    # Get cookie
+    session = requests.Session()
+    response = session.get(URL1)
+    cookie = session.cookies.get_dict()
+
+    # Get the available dates
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Cookie': f"cookie_accept=1; TVWebSession={cookie['TVWebSession']}",
+        'If-Modified-Since': 'Thu, 20 Apr 2023 18:47:29 GMT',
+        'Referer': 'https://termin.bremen.de/termine/select2?md=5',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
+        'sec-ch-ua': '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+    }
+
+    response = requests.get(URL1, headers=headers)
+
+    if response.status_code == 200:
+        data = response.content
+        print(data)
+    else:
+        print(f"Error: {response.status_code}")
+
+
+    # List of available dates
+
+    valid_date_list = []
+
 
 
 if __name__ == "__main__":
