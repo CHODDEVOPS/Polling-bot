4dc069785c73f9e7a53c40cbd7dde0a10769f54a
divinefavourodion <divineod9@gmail.com>
2023-04-26 20:51:30 +0200

    slight modifications for testing purposes

--- core/availability_notifier.py
+++ core/availability_notifier.py
@@ -1,6 +1,5 @@
 import sys
 from datetime import datetime, timedelta
-
 import requests
 from bs4 import BeautifulSoup
 from tenacity import retry, stop_after_attempt, wait_fixed
@@ -67,6 +66,9 @@ class TerminBremenScraper:
         if page == "mitte":
             self.first_page = "https://termin.bremen.de/termine/select2?md=5"
             self.second_page = "https://termin.bremen.de/termine/suggest?mdt=701&select_cnc=1&cnc-8568=0&cnc-8580=0&cnc-8582=0&cnc-8598=0&cnc-8583=0&cnc-8587=0&cnc-8597=0&cnc-8579=0&cnc-8596=0&cnc-8599=0&cnc-8600=0&cnc-8797=0&cnc-8790=1&cnc-8588=0&cnc-8591=0&cnc-8798=0&cnc-8569=0&cnc-8570=0&cnc-8571=0&cnc-8572=0&cnc-8574=0&cnc-8576=0&cnc-8595=0&cnc-8573=0&cnc-8593=0&cnc-8789=0&cnc-8577=0&cnc-8575=0&cnc-8578=0&cnc-8590=0&cnc-8585=0&cnc-8581=0"
+        elif page == "polizei":
+            self.first_page = "https://termin.bremen.de/termine/select2?md=13"
+            self.second_page = "https://termin.bremen.de/termine/suggest?mdt=704&select_cnc=1&cnc-8662=1&Strafanzeige+erstatten+=next+1"
         else:
             self.first_page = "https://termin.bremen.de/termine/select2?md=6"
             self.second_page = "https://termin.bremen.de/termine/suggest?mdt=702&select_cnc=1&cnc-8626=0&cnc-8627=0&cnc-8628=0&cnc-8650=0&cnc-8604=0&cnc-8629=0&cnc-8633=0&cnc-8619=0&cnc-8647=0&cnc-8651=0&cnc-8652=0&cnc-8801=0&cnc-8800=0&cnc-8793=1&cnc-8637=0&cnc-8639=0&cnc-8605=0&cnc-8606=0&cnc-8607=0&cnc-8608=0&cnc-8609=0&cnc-8610=0&cnc-8611=0&cnc-8613=0&cnc-8631=0&cnc-8649=0&cnc-8603=0&cnc-8616=0&cnc-8620=0&cnc-8621=0&cnc-8622=0&cnc-8623=0&cnc-8624=0&cnc-8648=0&cnc-8630=0&cnc-8632=0&cnc-8634=0&cnc-8612=0&cnc-8641=0&cnc-8642=0&cnc-8643=0&cnc-8644=0&cnc-8645=0&cnc-8601=0&cnc-8602=0&cnc-8617=0&cnc-8615=0&cnc-8614=0&cnc-8618=0&cnc-8625=0&cnc-8653=0&cnc-8646=0"
@@ -95,11 +97,24 @@ class TerminBremenScraper:
         """
         Parse the available dates and time slots from the HTML content.
         """
-        datetime_list = []
+        date_time_dict = {}
         doc = BeautifulSoup(page_content, "html.parser")
 
         # Find all h3 elements with the dates
-        date_elements = doc.find_all("h3", {"class": "ui-accordion-header"})
+        # date_elements = doc.find_all("h3", {"class": "ui-accordion-header"})
+        date_elements = doc.find_all("h3", {"title": True})
+        date_elements = date_elements[1:]
+
+        # Dictionary for German day of the week to English
+        german_to_english_days = {
+            "Montag": "Monday",
+            "Dienstag": "Tuesday",
+            "Mittwoch": "Wednesday",
+            "Donnerstag": "Thursday",
+            "Freitag": "Friday",
+            "Samstag": "Saturday",
+            "Sonntag": "Sunday",
+        }
 
         if not date_elements:
             print("Dates not found")
@@ -108,8 +123,12 @@ class TerminBremenScraper:
                 # Get the date text
                 date_text = date_element.text.strip()
 
+                # Replace German day of the week with English
+                for german_day, english_day in german_to_english_days.items():
+                    date_text = date_text.replace(german_day, english_day)
+
                 # Convert the date text to a datetime object
-                date_obj = datetime.strptime(date_text, "%A, %m/%d/%Y")
+                date_obj = datetime.strptime(date_text, "%A, %d.%m.%Y")
 
                 # Find the next sibling (the div containing the time table)
                 time_table_div = date_element.find_next_sibling("div")
@@ -118,22 +137,21 @@ class TerminBremenScraper:
                 time_table = time_table_div.find("table", {"class": "sugg_table"})
                 time_buttons = time_table.find_all("button", {"class": "suggest_btn"})
 
+                time_list = []
+
                 for btn in time_buttons:
-                    # Get the time text
-                    time_text = btn.text.strip()
+                    if not btn.has_attr('disabled'):
+                        # Get the time text
+                        time_text = btn.text.strip()
 
-                    # Convert the time text to a datetime object (with today's date)
-                    time_obj = datetime.strptime(time_text, "%H:%M")
+                        # Add the time text to the list
+                        time_list.append(time_text)
 
-                    # Combine the date and time into a single datetime object
-                    datetime_obj = date_obj.replace(
-                        hour=time_obj.hour, minute=time_obj.minute
-                    )
+                # Add the date object and time list to the dictionary
+                date_time_dict[date_obj.date()] = time_list
 
-                    # Add the datetime object to the list
-                    datetime_list.append(datetime_obj)
+        return date_time_dict
 
-        return datetime_list
 
     @retry(
         stop=stop_after_attempt(10),
@@ -142,11 +160,11 @@ class TerminBremenScraper:
             "Failed to retrieve dates after 10 trials."
         ),
     )
-    def run(self):
+    def run(self, page):
         """
-        Run the scraper with tenacity retries (10 times every 30 seconds).
+        Run the scraper with tenacity retries (retry every 30 seconds 10 times).
         """
-        available_dates = self.get_available_dates()
+        available_dates = self.get_available_dates(page)
         if not available_dates:
             print(
                 "The dates are not available on the website. Trying again in 60 seconds"
@@ -158,4 +176,4 @@ class TerminBremenScraper:
 
 if __name__ == "__main__":
     scraper = TerminBremenScraper()
-    scraper.run(page="mitte")
+    scraper.run(page="polizei")
