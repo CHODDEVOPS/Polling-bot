db41a5f794509a779e588c4f35c7cdd90c1e36c2
Mikhail Liamets <m.liamets@jacobs-university.de>
2023-06-01 21:09:24 +0200

    Sketch user repository

---
+++ core/repository/firebase.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+from ast import List
+
+import firebase
+import firebase_admin
+
+from firebase_admin import firestore
+
+
+class BaseFirebaseRepository:
+
+    def __init__(self, credentials: dict) -> None:
+        cred = firebase.credentials.Certificate(credentials)
+        self.app = firebase_admin.initialize_app(cred)
+
+
+class UserRepository(ABC):
+
+
+    @abstractmethod
+    def get_list_of_users(self) -> List[int]:
+        ...
+
+    @abstractmethod
+    def create_user(self, chat_id: int):
+        ...
+
+
+class UserFirebaseRepository(BaseFirebaseRepository, UserRepository):
+
+    db_name: str = "burger-users"
+
+    def __init__(self, credentials: dict) -> None:
+        super().__init__(credentials)
+        self.client = firestore.client()
+        self.collection = firestore.collection(self.db_name)
+
+    def get_list_of_users(self):
+        return self.collection.stream()
+
+    def create_user(self, chat_id: int):
+        pass
+
+    
